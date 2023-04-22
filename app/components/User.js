d2f419e4d04dfd0ac429baa0e29da5873b822877
Son Do <dominhson580@gmail.com>
2023-04-22 10:55:13 +0700

    Use useEffect and useReducer

--- app/components/User.js
+++ app/components/User.js
@@ -1,64 +1,106 @@
-import React from 'react'
-import queryString from 'query-string'
-import { fetchUser, fetchPosts } from '../utils/api'
-import Loading from './Loading'
-import { formatDate } from '../utils/helpers'
-import PostsList from './PostsList'
+import React, { useEffect, useReducer } from "react";
+import queryString from "query-string";
 
-export default class User extends React.Component {
-  state = {
-    user: null,
-    loadingUser: true,
-    posts: null,
-    loadingPosts: true,
-    error: null,
+import { fetchUser, fetchPosts } from "../utils/api";
+import { formatDate } from "../utils/helpers";
+
+import Loading from "./Loading";
+import PostsList from "./PostsList";
+
+const initialState = {
+  user: null,
+  loadingUser: true,
+  posts: null,
+  loadingPosts: true,
+  error: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "FETCH_PENDING":
+      return initialState;
+
+    case "FETCH_USER_SUCCESS":
+      return {
+        ...state,
+        error: null,
+        loadingUser: false,
+        user: action.user,
+      };
+
+    case "FETCH_POSTS_SUCCESS":
+      return {
+        ...state,
+        error: null,
+        loadingPosts: false,
+        posts: action.posts,
+      };
+
+    case "FETCH_ERROR":
+      return {
+        ...state,
+        error: action.error,
+        loadingUser: false,
+        loadingPosts: false,
+      };
+
+    default:
+      return state;
   }
-  componentDidMount() {
-    const { id } = queryString.parse(this.props.location.search)
+}
+
+export default function User({ location }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_PENDING" });
+
+    const { id } = queryString.parse(location.search);
 
     fetchUser(id)
       .then((user) => {
-        this.setState({ user, loadingUser: false})
+        dispatch({ type: "FETCH_USER_SUCCESS", user });
 
-        return fetchPosts(user.submitted.slice(0, 30))
+        return fetchPosts(user.submitted.slice(0, 30));
       })
-      .then((posts) => this.setState({
-        posts,
-        loadingPosts: false,
-        error: null
-      }))
-      .catch(({ message }) => this.setState({
-        error: message,
-        loadingUser: false,
-        loadingPosts: false
-      }))
-  }
-  render() {
-    const { user, posts, loadingUser, loadingPosts, error } = this.state
+      .then((posts) => dispatch({ type: "FETCH_POSTS_SUCCESS", posts }))
+      .catch(({ message }) =>
+        dispatch({ type: "FETCH_ERROR", error: message })
+      );
+  }, []);
 
-    if (error) {
-      return <p className='center-text error'>{error}</p>
-    }
+  const { user, posts, loadingUser, loadingPosts, error } = state;
 
-    return (
-      <React.Fragment>
-        {loadingUser === true
-          ? <Loading text='Fetching User' />
-          : <React.Fragment>
-              <h1 className='header'>{user.id}</h1>
-              <div className='meta-info-light'>
-                <span>joined <b>{formatDate(user.created)}</b></span>
-                <span>has <b>{user.karma.toLocaleString()}</b> karma</span>
-              </div>
-              <p dangerouslySetInnerHTML={{__html: user.about}} />
-            </React.Fragment>}
-        {loadingPosts === true
-          ? loadingUser === false && <Loading text='Fetching posts'/>
-          : <React.Fragment>
-              <h2>Posts</h2>
-              <PostsList posts={posts} />
-            </React.Fragment>}
-      </React.Fragment>
-    )
+  if (error) {
+    return <p className="center-text error">{error}</p>;
   }
+
+  return (
+    <>
+      {loadingUser ? (
+        <Loading text="Fetching User" />
+      ) : (
+        <>
+          <h1 className="header">{user.id}</h1>
+          <div className="meta-info-light">
+            <span>
+              joined <b>{formatDate(user.created)}</b>
+            </span>
+            <span>
+              has <b>{user.karma.toLocaleString()}</b> karma
+            </span>
+          </div>
+          <p dangerouslySetInnerHTML={{ __html: user.about }} />
+        </>
+      )}
+      {loadingPosts ? (
+        !loadingUser && <Loading text="Fetching posts" />
+      ) : (
+        <>
+          <h2>Posts</h2>
+          <PostsList posts={posts} />
+        </>
+      )}
+    </>
+  );
 }
