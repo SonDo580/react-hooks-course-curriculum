7e9ddc1f66a3a5342fea0273fb9aa56f650367b3
Son Do <dominhson580@gmail.com>
2023-04-22 09:02:19 +0700

    Don't extract ThemeConsumer

--- app/contexts/theme.js
+++ app/contexts/theme.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const { Consumer, Provider } = React.createContext()
+const ThemeContext = React.createContext()
 
-export const ThemeConsumer = Consumer
-export const ThemeProvider = Provider
+export const ThemeProvider = ThemeContext.Provider
+export default ThemeContext
